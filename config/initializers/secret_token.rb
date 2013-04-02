373a676323f769cca8da94eaa5c7a6e2926fc980
bhennes2 <bhennes2@gmail.com>
2013-04-01 19:11:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unternship::Application.config.secret_token = 'f5c77d7e6ddc9add7232618a36ba42feebcf7ad1127b037e6baeada5a9b8cb981050f34b1db59ce6405922d5eeb3741f7ed5a5f0b6dc61f3d77d95a49fbe2b98'
