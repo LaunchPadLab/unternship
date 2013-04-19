e945b1f10fb2a610cf18217503bb317883c8e8f3
bhennes2 <bhennes2@gmail.com>
2013-04-18 23:04:50 -0500

    Added placeholder config for mailers

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,15 @@ module Unternship
     
     config.assets.initialize_on_precompile = false
     
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => 'unternship.com',
+      :user_name            => ENV['username'],
+      :password             => ENV['password'],
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
+    
   end
 end
